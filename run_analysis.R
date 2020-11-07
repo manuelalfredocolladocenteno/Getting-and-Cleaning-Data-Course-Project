70b0fdbc3dc252315a3c3e7ebe397239ed3a113f
manuelalfredocolladocenteno <manuelalfredo.collado.centeno@alumnos.upm.es>
2020-11-07 10:43:11 +0100

    full project added

---
+++ run_analysis.R
@@ -0,0 +1,61 @@
+library(plyr)
+library(dplyr)
+
+# TODO Descargar el zip mediante instrucciones en R.
+
+# Read X
+X_train <- read.table("train/X_train.txt")
+X_test <- read.table("test/X_test.txt")
+X <- join(X_train, X_test, type = "full")
+
+# Read y
+y_train <- read.table("train/y_train.txt")
+y_test <- read.table("test/y_test.txt")
+y <- bind_rows(y_train, y_test)
+
+# Read subject
+subject_train <- read.table("train/subject_train.txt")
+subject_test <- read.table("test/subject_test.txt")
+subject <- bind_rows(subject_train, subject_test)
+names(subject) <- "subject"
+
+# Read features
+features <- read.table("features.txt")
+# Only mean or std features
+featuresX <- X[,grep("mean()|std()", features$V2)]
+
+# Descriptive activity names to name the activities in the data set
+activity_labels <- read.table("activity_labels.txt")
+set_activity <- function(data, label){
+  result <- data
+  for(activity in 1:6){
+    result <- replace(result[1], result[1]==activity, label[activity,2])
+    
+  }
+  
+  result
+}
+
+y_labeled <- set_activity(y, activity_labels)
+names(y_labeled) <- "activity"
+
+
+feats <- features[grep("mean()|std()", features$V2),][[2]]
+
+# Descriptive variable names
+feats <- gsub('^f', "frecuency", feats)
+feats <- gsub('^t', "time", feats)
+feats <- gsub('Acc', 'Acceleration', feats)
+feats <- gsub('BodyBody', 'Body', feats)
+
+names(featuresX) <- feats
+
+df <- bind_cols(subject, y_labeled, featuresX)
+
+# Independent tidy data set with the average of each variable 
+# for each activity and each subject
+tidy_dataset <- df %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+
+write.table(tidy_dataset, "tidy_dataset.txt", row.name=FALSE)
