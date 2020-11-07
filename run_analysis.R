f8dd37c9d74c1cc87812d0fdfbdbf613ff485e98
manuelalfredocolladocenteno <manuelalfredo.collado.centeno@alumnos.upm.es>
2020-11-07 11:45:39 +0100

    code changes

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,36 @@
+# Needed packages
 library(plyr)
 library(dplyr)
 
-# TODO Descargar el zip mediante instrucciones en R.
+# Download file
+temp<-tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+              destfile = temp)
+unzip(temp)
 
 # Read X
-X_train <- read.table("train/X_train.txt")
-X_test <- read.table("test/X_test.txt")
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 X <- join(X_train, X_test, type = "full")
 
 # Read y
-y_train <- read.table("train/y_train.txt")
-y_test <- read.table("test/y_test.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
 y <- bind_rows(y_train, y_test)
 
 # Read subject
-subject_train <- read.table("train/subject_train.txt")
-subject_test <- read.table("test/subject_test.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 subject <- bind_rows(subject_train, subject_test)
 names(subject) <- "subject"
 
 # Read features
-features <- read.table("features.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
 # Only mean or std features
 featuresX <- X[,grep("mean()|std()", features$V2)]
 
 # Descriptive activity names to name the activities in the data set
-activity_labels <- read.table("activity_labels.txt")
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 set_activity <- function(data, label){
   result <- data
   for(activity in 1:6){
@@ -45,7 +50,9 @@ feats <- features[grep("mean()|std()", features$V2),][[2]]
 # Descriptive variable names
 feats <- gsub('^f', "frecuency", feats)
 feats <- gsub('^t', "time", feats)
-feats <- gsub('Acc', 'Acceleration', feats)
+feats <- gsub('Acc', 'Accelerometer', feats)
+feats <- gsub('Gyro', 'Gyroscope', feats)
+feats <- gsub('Mag', 'Magnitude', feats)
 feats <- gsub('BodyBody', 'Body', feats)
 
 names(featuresX) <- feats
